53b98f0682229639934dd3d97b3989bbcd5fd663
vsogrin <vsogrin@phoenixit.ru>
2019-04-13 19:44:34 +0300

    git init

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,37 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
+
+import 'primereact/resources/themes/nova-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Home from "../components/Home";
+
+console.log('Hello World from Webpacker');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <Home />,
+        document.getElementById('root')
+    )
+});
