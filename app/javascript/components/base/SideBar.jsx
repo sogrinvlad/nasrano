196add28836c87736718e8da6011ad8c8f207819
vsogrin <vsogrin@phoenixit.ru>
2019-05-18 16:54:43 +0300

    devise

--- app/javascript/components/base/SideBar.jsx
+++ app/javascript/components/base/SideBar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default class SideBar extends React.Component {
-
     render() {
         return (
             <div className={'sidebar'}>
--- app/javascript/components/base/TopBar.jsx
+++ app/javascript/components/base/TopBar.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import {Button} from "primereact/button";
 
 export default class TopBar extends React.Component {
+
     render() {
         return(
             <div className="topbar">
                 Насрано логотип
+
+                <a href={'/users/sign_out'}>
+                    <Button label={'Выйти'} className='p-button-warning'/>
+                </a>
             </div>
         )
 
