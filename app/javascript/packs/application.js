2b07c6918f1fc5e55ad2b6b4164fa28a9abc8f11
vsogrin <vsogrin@phoenixit.ru>
2019-05-19 14:23:28 +0300

    ch bootstrap to CDN

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,7 +14,6 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react'
 import ReactDOM from 'react-dom'
